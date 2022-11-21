f6d04af546eae2a1cad5e7c8c347ef72c2e5b665
Melake12 <wubshetmelake@gmail.com>
2022-11-21 10:45:55 +0300

    feat: home banner

---
+++ components/Banner.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import React from 'react'
+
+type Props = {}
+
+function Banner({}: Props) {
+  return (
+    <div className='relative h-[300px] sm:h-[400px] 
+    lg:h-[500px] 2xl:h-[700px]'>
+        <Image
+        src="https://links.papareact.com/0fm"
+        alt='Banner Image'
+        fill
+        style={{
+            objectFit:'cover'
+        }}
+        />
+    <div className='absolute top-1/2 w-full text-center'>
+        <p className='text-sm sm:text-lg'>Not sure where to go? Perfect.</p>
+        <button className='text-purple-500 bg-white 
+        px-10 py-4 shadow-md rounded-full cursor-pointer 
+        hover:shadow-xl active:scale-90 duration-150
+        font-bold my-3'>I'm flexible</button>
+    </div>
+    </div>
+  )
+}
+
+export default Banner
