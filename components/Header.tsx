e5c6d2eecaa3d45fb0cbdcdb7f2faa05351807d5
Melake12 <wubshetmelake@gmail.com>
2022-11-21 15:34:05 +0300

    feat: header handle search

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,38 @@
 import Image from 'next/image'
-import React from 'react'
-import { MagnifyingGlassIcon, GlobeAltIcon, Bars3Icon, UserCircleIcon} from '@heroicons/react/24/solid'
+import React, { useState } from 'react'
+import { UsersIcon, MagnifyingGlassIcon, GlobeAltIcon, Bars3Icon, UserCircleIcon } from '@heroicons/react/24/solid'
 
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 type Props = {}
 
 export default function Header({ }: Props) {
+    const [searchInput, setSearchInput] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [numberOfGuests, setNumberOfGuests] = useState(1);
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection',
+    };
+
+
+
+    function handleSelect(ranges: any) {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+
+    const resetSearch = () => {
+        setSearchInput("");
+        setStartDate(new Date());
+        setEndDate(new Date());
+        setNumberOfGuests(1);
+    }
+
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5'>
             {/* Left */}
@@ -24,7 +51,10 @@ export default function Header({ }: Props) {
             {/* Middle */}
             <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
                 <input
-                    className='flex-grow pl-5 bg-transparent outline-none'
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    className='flex-grow pl-5 bg-transparent outline-none first-letter 
+                    text-xs text-gray-600 placeholder-gray-400'
                     type='text'
                     placeholder='Start your search'
                 />
@@ -34,13 +64,45 @@ export default function Header({ }: Props) {
 
             {/* Right */}
             <div className='flex space-x-4 items-center justify-end text-gray-500'>
-                <p className='hidden md:inline'>Become Host</p>
+                <p className='hidden md:inline'>Become Host </p>
                 <GlobeAltIcon className='h-6 cursor-pointer' />
                 <div className='flex space-x-2 items-center border-2 p-2 rounded-full'>
                     <Bars3Icon className='h-6' />
                     <UserCircleIcon className='h-6' />
                 </div>
             </div>
+
+            {/* Search Popup */}
+            {
+                searchInput &&
+                <div className='flex flex-col col-span-3 mx-auto'>
+
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        onChange={handleSelect}
+                        minDate={new Date()}
+                        rangeColors={["#FD5B61"]}
+                    />
+
+                    <div className='flex items-center border-b mb-4'>
+                        <h2 className='text-2xl font-semibold flex-grow'>Number of Guests</h2>
+                        <UsersIcon className='h-5' />
+                        <input value={numberOfGuests} min={1} type="number" className='w-12 pl-2 text-lg outline-none text-red-400' onChange={(e) => setNumberOfGuests(Number(e.target.value))} />
+                    </div>
+                    
+                    {/* Buttons */}
+                    <div className='flex justify-around'>
+                        <button onClick={resetSearch} className='text-gray-500'>
+                            Cancel
+                        </button>
+
+                        <button className='text-red-400'>
+                            Search
+                        </button>
+                    </div>
+
+                </div>
+            }
         </header>
     )
 }
